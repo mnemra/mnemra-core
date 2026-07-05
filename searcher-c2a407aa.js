62574c1b828d371c3814188a801d7b5380c0df41
prmanahan <prmanahan@users.noreply.github.com>
2026-07-05 02:10:28 +0000

    deploy: 0c5d5de7770621901342f2f317a0b119f7938b20

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9f0fba51.js',
+                path_to_root + 'searchindex-f8c1a881.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
